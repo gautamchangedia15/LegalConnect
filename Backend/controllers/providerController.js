83768485af9e8f0ecf912249a8129e04ddb132f1
Gautam <gautamchangedia2003@gmail.com>
2024-04-10 03:36:30 +0530

    get LSP list API

--- Backend/controllers/providerController.js
+++ Backend/controllers/providerController.js
@@ -1,17 +1,40 @@
 import admin from "../firebase.js";
 
 const db = admin.firestore();
+const providerCollection = db.collection("providers");
 
+const getAll = async (req, res) => {
+  //Building query based on available paramters passed by req
+  let query = providerCollection;
+  for (const paraName in req.query) {
+    //as expertise_area is a array
+    if (paraName === "expertise_area") {
+      query = query.where(paraName, "array-contains", req.query[paraName]);
+    } else {
+      query = query.where(paraName, "==", req.query[paraName]);
+    }
+  }
 
-//to do: create getAll APi for client and also for diff queries
-
+  //Retriving the data based upon query from database
+  try {
+    const querySnapshot = await query.get();
+    const providers = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    res.json(providers);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error retrieving provider data");
+  }
+};
 // add a new legal service provider
 // expertise area should store an arrays
 // isVerified->bool, phone_number, address,availability, profile_image_url and other data of LSP be added
 const addProfile = async (req, res) => {
   try {
     const { name, email, expertise_area } = req.body;
-    const docRef = await db.collection("providers").add({
+    const docRef = await providerCollection.add({
       name,
       email,
       expertise_area,
@@ -22,7 +45,7 @@ const addProfile = async (req, res) => {
     });
   } catch (error) {
     console.error("Error submitting provider:", error);
-    res.status(500).json({ error: "Error submitting provider data" });
+    res.status(500).json("Error submitting provider");
   }
 };
 
@@ -30,7 +53,7 @@ const addProfile = async (req, res) => {
 const getProfile = async (req, res) => {
   try {
     const providerId = req.params.id;
-    const providerDoc = await db.collection("providers").doc(providerId).get();
+    const providerDoc = await providerCollection.doc(providerId).get();
 
     if (!providerDoc.exists) {
       res.status(404).send("User not found");
@@ -44,4 +67,4 @@ const getProfile = async (req, res) => {
   }
 };
 
-export {addProfile,getProfile}
+export { getAll, addProfile, getProfile };
---
+++ Backend/routes/provider.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { getAll,addProfile,getProfile } from "../controllers/providerController.js";
+
+const router = express.Router();
+
+//get LSP based upon search filters
+router.route("/getAll").get(getAll)
+//add LSP
+router.route("/addProfile").post(addProfile);
+
+//get the details of LSP by passing the DocID of that LSP
+router.route("/getProfile/:id").get(getProfile);
+
+export default router;
